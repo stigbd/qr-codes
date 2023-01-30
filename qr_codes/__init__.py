6b66d26fcaa95c0bd7612fb7d0b52e8238fc8051
Stig B. Dørmænen <stigbd@gmail.com>
2023-01-30 13:53:53 +0100

    chore: Add tests and coverage monitoring Fixes #1

---
+++ qr_codes/__init__.py
@@ -0,0 +1,1 @@
+"""Module for the qr_codes package."""
--- qr_codes/cli.py
+++ qr_codes/cli.py
@@ -1,23 +1,23 @@
+"""Command line interface for QR code encoder/decoder."""
 import click
-
+from PIL import Image, UnidentifiedImageError
 from pyzbar.pyzbar import decode as zbar_decode, PyZbarError
-from PIL import Image
 import qrcode
 
 
 @click.group()
-def cli():
+def cli() -> None:
+    """QR code encoder/decoder."""
     pass
 
 
 @cli.command()
 @click.argument("filename", type=click.Path(exists=True))
-def decode(filename):
-    """Decode QR code from FILENAME
+def decode(filename) -> None:
+    """Decode QR code from FILENAME.
 
     FILENAME: path to image file
     """
-
     try:
         codes = zbar_decode(Image.open(filename))
         print(f"number of codes in image: {len(codes)}")
@@ -26,39 +26,34 @@ def decode(filename):
             print(f"data(decoded): >{data}<")
             print(f"type: {code.type}")
             print()
-    except FileNotFoundError:
-        print("File not found")
-    except PyZbarError as e:
-        print(f"Error reading QR code: {e}")
-    except Exception as e:
-        print(f"Error: {e}")
+    except PyZbarError as e:  # pragma: no cover
+        print(f"Error: Error decoding QR code: {e}")
+        exit(2)
+    except UnidentifiedImageError as e:
+        print(f"Error: Error opening image file: {e}")
+        exit(2)
 
 
 @cli.command()
 @click.argument("filename", type=click.Path(exists=True))
-def encode(filename):
-    """Encode QR code from FILENAME
+def encode(filename) -> None:
+    """Encode QR code from FILENAME.
 
-    FILENAME: path to text file"""
-    try:
-        with open(filename, "r") as f:
-            data = f.read()
-            qr = qrcode.QRCode(
-                version=1,
-                error_correction=qrcode.constants.ERROR_CORRECT_L,
-                box_size=10,
-                border=4,
-            )
-            qr.add_data(data.strip())
-            qr.make(fit=True)
-            img = qr.make_image(fill_color="black", back_color="white")
-            img.save(f'{filename.split(".")[0]}.png')
-
-    except FileNotFoundError:
-        print("File not found")
-    except Exception as e:
-        print(f"Error: {e}")
+    FILENAME: path to text file
+    """
+    with open(filename, "r") as f:
+        data = f.read()
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(data.strip())
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save(f'{filename.split(".")[0]}.png')
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     cli()
